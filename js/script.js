3bd7349632c0105dfc4483042d1193145fe87f1a
Davi Campaner <campanerdavi@gmail.com>
2026-01-30 18:20:30 -0300

    creating mov

--- js/script.js
+++ js/script.js
@@ -5,18 +5,37 @@ const size = 30
 
 const snake = [
     { x:200, y: 200},
-    { x:230, y: 200},
-    { x:260, y: 200}
+    { x:230, y: 200}
 ]
 
+let direction = "right"
 
 const drawSnake = () => {
-    ctx.fillStyle = "purple"
+    ctx.fillStyle = "#9400D3"
     
-    snake.forEach((position) =>{
+    snake.forEach((position, index) =>{
+
+        if (index == snake.length - 1){
+            ctx.fillStyle = "#A020F0"
+
+        }
+
         ctx.fillRect(position.x, position.y, size, size)
     })
 
 }
 
+const moveSnake = () => {
+    const head = snake.at(-1)
+
+    snake.shift()
+
+    if (direction == "right"){
+        snake.push({ x: head.x + size, y: head.y})
+    }
+}
+
+setInterval(() => {
+moveSnake()
 drawSnake()
+}, 300)
