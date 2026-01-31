eeb4616f7b949aab740df005a1344b5b523c1aa2
Davi Campaner <campanerdavi@gmail.com>
2026-01-31 11:55:24 -0300

    drawing map

--- js/script.js
+++ js/script.js
@@ -3,10 +3,8 @@ const ctx = canvas.getContext("2d")
 
 const size = 30
 
-const snake = [
-    { x:200, y: 200},
-    { x:230, y: 200}
-]
+const snake = [{x: 270, y:240}]
+
 
 let direction, LoopId
 
@@ -49,10 +47,29 @@ const moveSnake = () => {
     snake.shift()
 }
 
+const drawGrid = () => {
+    ctx.lineWidth = 1
+    ctx.strokeStyle = "#191919"
+
+    for (let i = 30; i < canvas.width; i += 30) {
+        ctx.beginPath()
+        ctx.lineTo(i, 0)
+        ctx.lineTo(i, 600)
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.lineTo(0, i)
+        ctx.lineTo(600, i)
+        ctx.stroke()
+
+
+    }    
+}
+
 const gameLoop = () => {
     clearInterval(LoopId)
     ctx.clearRect(0, 0, 600, 600)
-
+    drawGrid()
     moveSnake()
     drawSnake()
 
