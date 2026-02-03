1a198528585fc263445540eb18f1c80367590fc6
Davi Campaner <campanerdavi@gmail.com>
2026-02-03 15:15:21 -0300

    adjusting drop with the map

--- js/script.js
+++ js/script.js
@@ -9,9 +9,14 @@ const randomNumber = (min, max) => {
     return Math.round(Math.random() * (max - min) + min)
 } 
 
+const randomPosition = () => {
+    const number = randomNumber(0, canvas.width - size)
+    return Math.round(number / 30) * 30
+}
+
 const food = {
-    x: randomNumber(0, 570),
-    y: randomNumber(0, 570),
+    x: randomPosition(0, 570),
+    y: randomPosition(0, 570),
     color: "yellow"
 }
 
