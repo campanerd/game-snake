6aafdd03b6b2acba535872c87a5822a27e26836a
Davi Campaner <campanerdavi@gmail.com>
2026-02-06 15:54:07 -0300

    adding audio while eating

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
 
+const audio = new Audio('assets/assets_audio.mp3')
+
 const size = 30
 
 const snake = [{x: 270, y:240}]
@@ -104,6 +106,7 @@ const chackEat = () => {
 
     if (head.x == food.x && head.y == food.y){
         snake.push(head)
+        audio.play()
 
         let x = randomPosition()
         let y = randomPosition()
