11571a5161924a1301fe5ea7328945541d98f9b1
Davi Campaner <campanerdavi@gmail.com>
2026-02-18 18:18:40 -0300

    changing initial snake size

--- js/script.js
+++ js/script.js
@@ -6,9 +6,15 @@ const finalScore = document.querySelector(".final-score > span")
 const menu = document.querySelector(".menu-screen")
 const buttonPlay = document.querySelector(".btn-play")
 
+const audio = new Audio("assets\assets_audio.mp3")
+
 const size = 30
 
-const snake = [{x: 270, y:240}]
+const snake = [
+    {x: 270, y:240},
+    {x: 300, y:240}
+
+]
 
 const randomNumber = (min, max) => {
     return Math.round(Math.random() * (max - min) + min)
